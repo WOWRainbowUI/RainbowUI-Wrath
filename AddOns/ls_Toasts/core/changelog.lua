d23ea581998b2de073f95a2cf0188658df1780ff
class2u <class2u@gmail.com>
2024-01-05 19:51:00 +0800

    更新 通知面板增強 30403.02
    # Conflicts:
    #	AddOns/ls_Toasts/locales/deDE.lua
    #	AddOns/ls_Toasts/locales/esES.lua
    #	AddOns/ls_Toasts/locales/frFR.lua
    #	AddOns/ls_Toasts/locales/itIT.lua
    #	AddOns/ls_Toasts/locales/koKR.lua
    #	AddOns/ls_Toasts/locales/ruRU.lua
    #	AddOns/ls_Toasts/locales/zhTW.lua

--- AddOns/ls_Toasts/core/changelog.lua
+++ AddOns/ls_Toasts/core/changelog.lua
@@ -6,8 +6,6 @@ local _G = getfenv(0)
 
 -- Mine
 E.CHANGELOG = [[
-- Added 3.4.2 support.
-- Added "Two Tone" variants to all built-in skins. They have a two tone background that's similar to that of Blizz
-  achievement toasts. I might tweak the textures depending on the feedback.
-- Updated Spanish translation. Translated by Shacoulrophobia@Curse.
+- Tweaked toast borders so that the texture looks more consistent with no warping or shifting.
+  Doesn't affect ElvUI skins.
 ]]
