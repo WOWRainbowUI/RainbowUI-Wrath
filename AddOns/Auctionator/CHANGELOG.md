31a594a50ffdd52d949e26643976d842145f0629
class2u <class2u@gmail.com>
2024-01-11 11:38:02 +0800

    更新 拍賣小幫手 10.2.12
    # Conflicts:
    #	AddOns/Auctionator/Auctionator.toc
    #	AddOns/Auctionator/Locales/deDE.lua
    #	AddOns/Auctionator/Locales/esES.lua
    #	AddOns/Auctionator/Locales/esMX.lua
    #	AddOns/Auctionator/Locales/frFR.lua
    #	AddOns/Auctionator/Locales/itIT.lua
    #	AddOns/Auctionator/Locales/koKR.lua
    #	AddOns/Auctionator/Locales/ptBR.lua
    #	AddOns/Auctionator/Locales/ruRU.lua
    #	AddOns/Auctionator/Locales/zhTW.lua

--- AddOns/Auctionator/CHANGELOG.md
+++ AddOns/Auctionator/CHANGELOG.md
@@ -1,8 +1,15 @@
 # Auctionator
 
-## [10.1.25](https://github.com/Auctionator/Auctionator/tree/10.1.25) (2023-10-12)
-[Full Changelog](https://github.com/Auctionator/Auctionator/compare/10.1.24...10.1.25) 
+## [10.2.12](https://github.com/Auctionator/Auctionator/tree/10.2.12) (2024-01-05)
+[Full Changelog](https://github.com/Auctionator/Auctionator/compare/10.2.11...10.2.12) 
 
-- Mainline: Selling: Fix wrong deposit for gear  
-- Classic: Buy: Fix row selection after stopping extended results loading  
-- Classic: Stop chain buy when reaching own auctions  
+- ResultsListing: Trucante header text when it is too long for the column width  
+- Change tooltip text for edit item icon for a shopping list  
+- Remove unused default toc file  
+- [Fixes #1313] Resolve constant overflow/missing shopping lists  
+    This was caused by doing full scans on a lot of different realms, this  
+    should no longer cause data loss. Solution is to serialize the realm  
+    data for any realms not currently in use.  
+- Retail: Update color picker code for 10.2.5  
+- Classic: Fix tooltip scan not always working when multiple done in sequence  
+- Selling: Bag/Groups: Ensure favourites are always visible in the customise view  
--- AddOns/Auctionator/Source/Groups/BagCache.lua
+++ AddOns/Auctionator/Source/Groups/BagCache.lua
@@ -52,10 +52,38 @@ local function KeyPartsItemLink(itemLink)
     parts[i] = ""
   end
 
+  local bonusIDStart = 14
   local numBonusIDs = tonumber(parts[14] or "") or 0
+  local modStart = bonusIDStart + numBonusIDs + 1
 
-  for i = 14 + numBonusIDs + 1, #parts do
-    parts[i] = nil
+  local numMods = tonumber(parts[modStart] or "") or 0
+  -- complicated way to only keep the modifiers that affect item level and scrap
+  -- any tmog/crafting details
+  -- Details of modifiers at https://warcraft.wiki.gg/wiki/ItemLink#Modifier_Types
+  if numMods > 0 then
+    local wantedMods = {}
+    for i = 1, numMods do
+      local id = tonumber(parts[modStart + i * 2 - 1])
+      -- timewalker level, artifact tier, pvp rating, dragonflight quality id
+      if id == 9 or id == 24 or id == 26 or id == 29 or id == 30 or id == 38 then
+        table.insert(wantedMods, id)
+        table.insert(wantedMods, parts[modStart + i * 2])
+      end
+    end
+    numMods = #wantedMods / 2
+    parts[modStart] = tostring(numMods)
+    for i = 1, #wantedMods do
+      parts[modStart + i] = tostring(wantedMods[i])
+    end
+  end
+  if numMods > 0 then
+    for i = modStart + numMods * 2 + 1, #parts do
+      parts[i] = nil
+    end
+  else
+    for i = modStart, #parts do
+      parts[i] = nil
+    end
   end
 
   return Auctionator.Utilities.StringJoin(parts, ":")
@@ -72,6 +100,14 @@ local function KeyPartsPetLink(itemLink)
 end
 
 local function GetItemKey(entry)
+  if Auctionator.Config.Get(Auctionator.Config.Options.SELLING_GROUP_BY_ITEM_KEY) then
+    local itemLevel = entry.itemLevel or 0
+    if entry.classID == Enum.ItemClass.Battlepet then
+      itemLevel = Auctionator.Utilities.GetPetLevelFromLink(entry.itemLink)
+    end
+    return entry.itemID .. "_" ..  entry.itemName .. "_" .. itemLevel .. "_" .. tostring(entry.auctionable)
+  end
+
   -- Battle pets
   if entry.classID == Enum.ItemClass.Battlepet then
     return "p:" .. KeyPartsPetLink(entry.itemLink)
